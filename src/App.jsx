7ef799e039223aac836ac0fa593b376108807aaf
jason Luke <realjason93@gmail.com>
2026-04-18 18:31:35 +0100

    did some stabilization

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Navbar from "./Components/Navba"
+import Navbar from "./Components/Navbar"
 
 import Hero from "./Components/Hero"
 import About from "./Components/About"
 import Skills from "./Components/Skills"
 import FeaturedProjects from "./Components/FeaturedProjects"
 import Contact from "./Components/Contact"
+
+
 
 
 function App() {
